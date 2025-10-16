1c5775a2135946b4a8a6be3f5e63561b8ff675e4
MortezaGhoddousi <djmgh1994@gmail.com>
2025-10-16 20:00:15 +0330

    added session 04 to Django 02

---
+++ Django 02/Session 04/main.py
@@ -0,0 +1,31 @@
+firstName = "Morteza"
+lastName = "Ghoddousi"
+
+age = 31
+weight = 83
+
+height = 1.86
+
+isMale = True
+
+print("Hello world!")
+print("Morteza")
+print(firstName)
+
+
+print(firstName, type(firstName))
+print(lastName, type(lastName))
+print(age, type(age))
+print(weight, type(weight))
+print(height, type(height))
+print(isMale, type(isMale))
+
+x = 4
+y = 5.5
+z = x + y
+print(z, type(z))
+
+
+
+
+
