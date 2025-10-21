c7ed7e86c05f56c1d79af916bf585c3b13e637d4
MortezaGhoddousi <djmgh1994@gmail.com>
2025-10-21 20:04:17 +0330

    added session 06 to Django 02

---
+++ Django 02/Session 06/main.py
@@ -0,0 +1,90 @@
+# if condition:
+#     if's body
+
+# age = 15
+
+# if age >= 18:
+#     print("adult")
+# else:
+#     print("child")
+
+# print("end of the script")
+
+# userAdmin = "Morteza"
+# passAdmin = "123"
+
+# username = input("enter username: ")
+# password = input("enter password: ")
+
+# if userAdmin == username and passAdmin == password:
+#     print("access granted!")
+# else:
+#     print("username or password is wrong! try again ...")
+
+
+
+# age = int(input("enter your age: "))
+
+# if age < 10:
+#     print("Child")
+#     print("first if's body")
+# elif 10<=age<=18:
+#     print("Teenager")
+#     print("second if's body")
+# elif 18<age<=40:
+#     print("Adult")
+#     print("third if's body")
+# else:
+#     print("Elder")
+#     print("else's body")
+
+
+# for variable in list:
+#     for's body
+
+# names = ["Morteza", "Nima", "Noush", "Mohsen", "Arman", "Setayesh"]
+
+# print(names)
+# names.append("Setayesh")
+# print(names)
+# names.insert(2, "Setayesh")
+# print(names)
+
+# print(names[0])
+# print(names[1])
+# print(names[2])
+# print(names[3])
+# print(names[4])
+
+# for n in names:
+#     print(n)
+#     print("end of the iteration")    
+
+# print("end of the for loop")
+# names.sort()
+# print(names)
+
+# def f(x):
+#     z = x+1
+#     return z
+
+
+
+# h = f(3)
+# print(h)
+
+
+a = input("a --> umber  :")
+b = input("b --> umber  :")
+c = input("c --> umber  :")
+
+def D(a , b, c):
+    d = b**2 - 4 * a * c
+    return d
+
+
+
+print(D)
+
+
+# if D > 0:
